e501fefe04977404604f098a0c7967d67a597cc0
Antonio <antojoseabad@gmail.com>
2026-03-13 17:03:52 +0100

    feat(pipes): add custom pipe to filter data

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -8,16 +8,18 @@ import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from "../../pipes/heroCreator-pipe";
 import { HeroSortByPipe } from '../../pipes/heroSortBy-pipe';
 import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/heroFilter-pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe, HeroFilterPipe],
   templateUrl: './custom-page.html',
 })
 export default class CustomPage { 
 
-  name = signal('Antonio Borrero')
+  name = signal('Antonio Borrero');
   upperCase = signal<boolean>(true);
-  heroes = signal(heroes)
-  sortBy = signal<keyof Hero | null>(null)
+  heroes = signal(heroes);
+  sortBy = signal<keyof Hero | null>(null);
+  searchQuery = signal('')
 }
